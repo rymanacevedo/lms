41af9cbfdcdd0dc624cc991a3b9615bdbe989f52
Ryan Acevedo <ryan.avocado@protonmail.com>
2025-03-28 03:35:23 +0000

    next js setup with drizzle configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import {defineConfig} from 'drizzle-kit';
+import { env } from '@/data/env/server';
+export default defineConfig({
+    out: './src/drizzle/migrations',
+    schema: './src/drizzle/schema.ts',
+    strict: true,
+    verbose: true,
+    dialect: 'postgresql',
+    dbCredentials: {
+        password: env.DB_PASSWORD,
+        user: env.DB_USER,
+        database: env.DB_NAME,
+        host: env.DB_HOST,
+        ssl: false
+
+    }
+});
